87e59030c391299ecaa66baeec1ba7161c3c8a34
minwang03 <quang.2174801030079@vanlanguni.vn>
2025-09-12 18:46:55 +0700

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,7 +23,12 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { sub: user._id, role: user.role, username: user.email };
+    const payload = {
+      sub: user._id,
+      role: user.role,
+      username: user.email,
+      isActive: user.isActive,
+    };
     return {
       access_token: this.jwtService.sign(payload),
     };
