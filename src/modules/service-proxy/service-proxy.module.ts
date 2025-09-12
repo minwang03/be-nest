c0d2f807b03f0f8928f512dfde36679c5532b36c
minwang03 <quang.2174801030079@vanlanguni.vn>
2025-09-12 15:36:05 +0700

    Update service-proxy.module.ts

--- src/modules/service-proxy/service-proxy.module.ts
+++ src/modules/service-proxy/service-proxy.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ServiceProxyService } from './service-proxy.service';
 import { ServiceProxyController } from './service-proxy.controller';
+import {
+  ServiceProxy,
+  ServiceProxySchema,
+} from './schemas/service-proxy.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: ServiceProxy.name, schema: ServiceProxySchema },
+    ]),
+  ],
   controllers: [ServiceProxyController],
   providers: [ServiceProxyService],
 })
