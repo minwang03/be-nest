6f8b4178eb624d8ed48aef4ecfb653cae49fb167
minwang03 <quang.2174801030079@vanlanguni.vn>
2025-09-26 14:20:26 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,14 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
-  app.setGlobalPrefix('api'); 
-  
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, 
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
