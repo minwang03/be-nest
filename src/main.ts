489a7e42022cc04ee565f5b208d58f1a6a98cfc8
minwang03 <quang.2174801030079@vanlanguni.vn>
2025-12-06 11:22:46 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
+  const port = process.env.PORT || configService.get('PORT') || 3000;
 
   app.setGlobalPrefix('api');
 
