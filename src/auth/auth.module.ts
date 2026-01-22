fdf66f9189e5ce7be3e84ce94bf3d2fbc34db5d7
minwang03 <quang.2174801030079@vanlanguni.vn>
2026-01-22 10:37:24 +0700

    u

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,11 +7,13 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { JwtStrategy } from '@/auth/passport/jwt.strategy';
 import { LocalStrategy } from '@/auth/passport/local.strategy';
+import { LoginSessionsModule } from '@/modules/login-sessions/login-sessions.module';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
+    LoginSessionsModule,
     JwtModule.registerAsync({
       useFactory: async (configService: ConfigService) => ({
         global: true,
--- src/modules/login-sessions/login-sessions.module.ts
+++ src/modules/login-sessions/login-sessions.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { LoginSessionsService } from './login-sessions.service';
 import { LoginSessionsController } from './login-sessions.controller';
+import { LoginSession, LoginSessionSchema } from './schemas/login-session.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: LoginSession.name, schema: LoginSessionSchema },
+    ]),
+  ],
   controllers: [LoginSessionsController],
   providers: [LoginSessionsService],
+  exports: [LoginSessionsService],
 })
 export class LoginSessionsModule {}
